d2fd6be758988898bba2c4a4448e7b68f5d84514
Will Soula <william.soula@starz.com>
2021-11-03 14:30:42 -0600

    feat: Get actual trivia

--- trivia_game.py
+++ trivia_game.py
@@ -1,5 +1,7 @@
 """ Output the request ratio for the last X weeks for PA-UE1 to each service """
 import logging
+import urllib
+import json
 from errbot import BotPlugin, botcmd, arg_botcmd
 from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 
@@ -15,10 +17,12 @@ class GuessFlows(BotFlow):
         """ This is a flow that can set a guessing game."""
         # setup Flow
         game_created = flow.connect('trivia', auto_trigger=True, room_flow=True)
-        one_guess = game_created.connect('guess')
+        question = game_created.connect('question')
+        one_guess = question.connect('guess')
         one_guess.connect(one_guess)  # loop on itself
         one_guess.connect(FLOW_END, predicate=lambda ctx: 'ended' in ctx)
         game_created.hints = False
+        question.hints = False
         one_guess.hints = False
 
 
@@ -28,18 +32,36 @@ class TriviaGame(BotPlugin):
     def trivia(self, msg, args):
         """ Get trivia questions """
         logger.debug('msg=%s\nargs=%s', msg, args)
-        trivias = []
-        trivias.append({'question': 'blah', 'incorrect': ['boo', 'hoo'], 'correct': ['bin']})
-        msg.ctx['trivias'] = trivias
-        return 'Questions Retrieved'
+        url = 'https://opentdb.com/api.php?amount=10'
+        page = urllib.request.Request(url)
+        response = json.loads(urllib.request.urlopen(page).read().decode('utf-8'))
+        if 'results' in response:
+            msg.ctx['trivias'] = response['results']
+            return 'Questions Retrieved'
+        return 'No questions returned'
+
+    @botcmd
+    def question(self, msg, args):
+        """ Get a question """
+        logger.debug('msg=%s\nargs=%s', msg, args)
+        if 'trivias' in msg.ctx:
+            msg.ctx['question'] = msg.ctx['trivias'][0]['question']
+            msg.ctx['correct_answer'] = msg.ctx['trivias'][0]['correct_answer']
+            msg.ctx['incorrect_answers'] = msg.ctx['trivias'][0]['incorrect_answers']
+            answers = ''
+            for answer in msg.ctx['incorrect_answers']:
+                answers = answers + answer + '\n'
+            answers = answers + msg.ctx['correct_answer']
+            return msg.ctx['question']+'\n'+answers
+        return 'Must initialize with trivia command first'
 
     @arg_botcmd('guess', type=str)
     def guess(self, msg, guess):
         """ Guess """
-        if 'trivias' in msg.ctx:
-            if guess == 'foo':
+        if 'trivias' in msg.ctx and 'correct_answer' in msg.ctx:
+            if guess == msg.ctx['correct_answer']:
                 msg.ctx['ended'] = True
                 return 'You got it!'
             else:
                 return guess+' was not it'
-        return 'Must initialize with trivia command first'
+        return 'Must initialize with trivia command and question command first'
