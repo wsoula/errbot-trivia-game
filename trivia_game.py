576d12ec1e09593d8cacfca821a1af586d38069e
Will Soula <william.soula@starz.com>
2021-11-03 19:55:06 -0600

    feat: Loop back to question and get more

--- trivia_game.py
+++ trivia_game.py
@@ -8,6 +8,8 @@ from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+TOTAL_QUESTIONS = 2
+
 
 class GuessFlows(BotFlow):
     """ Conversation flows related to polls"""
@@ -17,10 +19,11 @@ class GuessFlows(BotFlow):
         """ This is a flow that can set a guessing game."""
         # setup Flow
         game_created = flow.connect('trivia', auto_trigger=True, room_flow=True)
-        # question = game_created.connect('question', predicate=lambda ctx: 'trivias' in ctx)
+        # question = game_created.connect('question', predicate=lambda ctx: 'trivias' in ctx) # ctx is {}
         question = game_created.connect('question')
         one_guess = question.connect('guess')
-        one_guess.connect(one_guess)  # loop on itself
+        # one_guess.connect(one_guess)  # loop on itself
+        one_guess.connect(question, predicate=lambda ctx: 'correct' in ctx)  # loop on question
         one_guess.connect(FLOW_END, predicate=lambda ctx: 'ended' in ctx)
         game_created.hints = False
         question.hints = False
@@ -33,11 +36,12 @@ class TriviaGame(BotPlugin):
     def trivia(self, msg, args):
         """ Get trivia questions """
         logger.debug('msg=%s\nargs=%s', msg, args)
-        url = 'https://opentdb.com/api.php?amount=10'
+        url = 'https://opentdb.com/api.php?amount='+str(TOTAL_QUESTIONS)
         page = urllib.request.Request(url)
         response = json.loads(urllib.request.urlopen(page).read().decode('utf-8'))
         if 'results' in response:
             msg.ctx['trivias'] = response['results']
+            msg.ctx['index'] = 0
             return 'Questions Retrieved'
         return 'No questions returned'
 
@@ -46,9 +50,10 @@ class TriviaGame(BotPlugin):
         """ Get a question """
         logger.debug('msg=%s\nargs=%s', msg, args)
         if 'trivias' in msg.ctx:
-            msg.ctx['question'] = msg.ctx['trivias'][0]['question']
-            msg.ctx['correct_answer'] = msg.ctx['trivias'][0]['correct_answer']
-            msg.ctx['incorrect_answers'] = msg.ctx['trivias'][0]['incorrect_answers']
+            index = msg.ctx['index']
+            msg.ctx['question'] = msg.ctx['trivias'][index]['question']
+            msg.ctx['correct_answer'] = msg.ctx['trivias'][index]['correct_answer']
+            msg.ctx['incorrect_answers'] = msg.ctx['trivias'][index]['incorrect_answers']
             answers = ''
             for answer in msg.ctx['incorrect_answers']:
                 answers = answers + answer + '\n'
@@ -62,7 +67,10 @@ class TriviaGame(BotPlugin):
         """ Guess """
         if 'trivias' in msg.ctx and 'correct_answer' in msg.ctx:
             if guess == msg.ctx['correct_answer']:
-                msg.ctx['ended'] = True
+                msg.ctx['index'] = msg.ctx['index'] + 1
+                msg.ctx['correct'] = True
+                if msg.ctx['index'] == TOTAL_QUESTIONS:
+                    msg.ctx['ended'] = True
                 return 'You got it!'
             else:
                 return guess+' was not it'
